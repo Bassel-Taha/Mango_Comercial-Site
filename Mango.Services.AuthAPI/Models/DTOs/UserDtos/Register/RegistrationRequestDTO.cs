60557dd53a796fb0569375d0599c2fcf235b84bf
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2023-11-28 15:29:46 +0200

    some tweaks for the code for the better performance

--- Mango.Services.AuthAPI/Models/DTOs/UserDtos/Register/RegistrationRequestDTO.cs
+++ Mango.Services.AuthAPI/Models/DTOs/UserDtos/Register/RegistrationRequestDTO.cs
@@ -15,7 +15,7 @@ namespace Mango.Services.AuthAPI.Models.DTOs.UserDtos.Register
 
         public string Password { get; set; }
 
-        public string PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
 
         public string Role { get; set; }
     }
--- Mango.Web/Models/LoginRequestDTO.cs
+++ Mango.Web/Models/LoginRequestDTO.cs
@@ -5,7 +5,9 @@ namespace Mango.Web.Models
 {
     public class LoginRequestDTO
     {
+        [Required]
         public string UserName { get; set; }
+        [Required]
         [PasswordPropertyText]
         [StringLength(15, ErrorMessage = "password must be between 8 to 15 charachters", MinimumLength = 8)]
         public string Password { get; set; }
