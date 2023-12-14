a3bffe734d69df1da8ad9f1f64924519d4447fb0
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2023-12-14 19:32:37 +0200

    adding the the images and the roasted duck to the product database onmodelcreation

--- Mango.Services.ProductAPI/Data/AppDBContext.cs
+++ Mango.Services.ProductAPI/Data/AppDBContext.cs
@@ -22,10 +22,10 @@ namespace Mango.Services.ProductsAPI.Data
             modelBuilder.Entity<Products>().HasData(new Products
             {
                 ProductId = 1,
-                Name = "Samosa",
+                Name = "Sambosa",
                 Price = 15,
                 Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
-                ImageUrl = "https://placehold.co/603x403",
+                ImageUrl = "https://th.bing.com/th/id/R.4e7f53fcc6f1412363c9aa683dd1426c?rik=1KrgIdIJqrLnhQ&pid=ImgRaw&r=0&sres=1&sresct=1",
                 CategoryName = "Appetizer"
             });
             modelBuilder.Entity<Products>().HasData(new Products
@@ -34,7 +34,7 @@ namespace Mango.Services.ProductsAPI.Data
                 Name = "Paneer Tikka",
                 Price = 13.99,
                 Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
-                ImageUrl = "https://placehold.co/602x402",
+                ImageUrl = "https://th.bing.com/th/id/R.2d6497c8af8a41b39d52391c605141dc?rik=PZXUnfSWfAhYUg&pid=ImgRaw&r=0",
                 CategoryName = "Appetizer"
             });
             modelBuilder.Entity<Products>().HasData(new Products
@@ -43,7 +43,7 @@ namespace Mango.Services.ProductsAPI.Data
                 Name = "Sweet Pie",
                 Price = 10.99,
                 Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
-                ImageUrl = "https://placehold.co/601x401",
+                ImageUrl = "https://th.bing.com/th/id/OIP.U2VOKNY3f8Os9ISq6PoG8gHaHU?w=628&h=621&rs=1&pid=ImgDetMain",
                 CategoryName = "Dessert"
             });
             modelBuilder.Entity<Products>().HasData(new Products
@@ -52,8 +52,17 @@ namespace Mango.Services.ProductsAPI.Data
                 Name = "Pav Bhaji",
                 Price = 15,
                 Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
-                ImageUrl = "https://placehold.co/600x400",
+                ImageUrl = "https://th.bing.com/th/id/R.7d1656d63d1f2cd751cd8cb145cbbcc2?rik=L2PKxpb9MrlXzQ&pid=ImgRaw&r=0",
                 CategoryName = "Entree"
+            });
+            modelBuilder.Entity<Products>().HasData(new Products
+            {
+                ProductId = 5,
+                Name = "Roasted Duck",
+                Price = 20.00,
+                Description = "Roast Duck has tender and juicy meat, crispy skin, and it's glazed with the honey-balsamic glaze to give the duck a beautiful roasted look.",
+                ImageUrl = "https://th.bing.com/th/id/OIP.Qbkxc5tV-o4BXXgu05QThgHaEL?rs=1&pid=ImgDetMain",
+                CategoryName = "Main Dish"
             });
         }
     }
