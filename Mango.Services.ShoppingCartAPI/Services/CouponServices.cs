bffc059d5e1611e8aa871aeeb1de75cfb54e16cb
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2023-12-22 19:32:12 +0200

    adding the IcouponsService and its implementation to get the Coupons from the CouponApi (BUT i'll need refactoring to remove the token string from the parameters and let it run automatically from the web api)

---
+++ Mango.Services.ShoppingCartAPI/Services/CouponServices.cs
@@ -0,0 +1,54 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Services
+{
+    using System.Configuration;
+    using Mango.Services.ShoppingCartAPI.Model.DTO;
+    using Mango.Services.ShoppingCartAPI.Services.IServices;
+    using Newtonsoft.Json;
+
+    using ConfigurationManager = Microsoft.Extensions.Configuration.ConfigurationManager;
+
+    public class CouponServices : ICouponService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public CouponServices(IHttpClientFactory clientFactory)
+        {
+            this._clientFactory = clientFactory;
+        }
+
+
+        //must be refactored later to get the token from the authapi automaticly
+        public async Task<ResponsDTO> GetAllCoupons(string token)
+        {
+            try
+            {
+                var client = this._clientFactory.CreateClient("Coupons");
+                var httpmessage = new HttpRequestMessage();
+                httpmessage.Headers.Add("Authorization", $"bearer {token}");
+                var baseUrl = SD.CouponApi;
+                httpmessage.RequestUri = new Uri($"{baseUrl}/api/Coupon/GetAll");
+                var messageresponse = await client.SendAsync(httpmessage);
+                var serializedApiResponse = await messageresponse.Content.ReadAsStringAsync();
+                var apiResponse = JsonConvert.DeserializeObject<ResponsDTO>(serializedApiResponse);
+
+                if (apiResponse.IsSuccess != false)
+                {
+                    var result = JsonConvert.DeserializeObject<List<Coupon>>(apiResponse.Result.ToString());
+                    var responsedto = new ResponsDTO() { IsSuccess = true, Message = "", Result = result };
+                    return responsedto;
+                }
+
+                apiResponse.IsSuccess = false;
+                return apiResponse;
+            }
+            catch (Exception e)
+            {
+                var reponse = new ResponsDTO();
+                reponse.IsSuccess = false;
+                reponse.Message = e.Message;
+                return reponse;
+            }
+        }
+
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Services/IServices/ICouponService.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Services.IServices
+{
+    using Mango.Services.ShoppingCartAPI.Model.DTO;
+
+    public interface ICouponService
+    {
+        public Task<ResponsDTO> GetAllCoupons(string token);
+    }
+}
