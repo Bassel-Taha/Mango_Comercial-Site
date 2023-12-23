9d25a5b7b9ce1e3abad30b78362786818713acd5
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2023-12-23 22:04:44 +0200

    correcting teh name of the variable accessor

--- Mango.Web/services/TockenProvider.cs
+++ Mango.Web/services/TockenProvider.cs
@@ -5,29 +5,29 @@ namespace Mango.Web.services
 {
     public class TockenProvider : ITockenProvider
     {
-        private readonly IHttpContextAccessor _accessot;
-        public TockenProvider(IHttpContextAccessor accessot)
+        private readonly IHttpContextAccessor _accessor;
+        public TockenProvider(IHttpContextAccessor accessor)
         {
-            _accessot = accessot;
+            this._accessor = accessor;
         }
         //clear all the cookies and the token to be used when signing out
         public void ClearToken()
         {
-            _accessot.HttpContext?.Response.Cookies.Delete(SD.TockenCookie);
+            this._accessor.HttpContext?.Response.Cookies.Delete(SD.TockenCookie);
         }
 
         public string? GetToken()
         {
             string? token = null;
             //getting the token from the cookie if there is one if not return null
-            bool? hastoken = _accessot.HttpContext?.Request.Cookies.TryGetValue(SD.TockenCookie, out token);
+            bool? hastoken = this._accessor.HttpContext?.Request.Cookies.TryGetValue(SD.TockenCookie, out token);
             // if there is a token return it if not return null
             return hastoken is true ? token : null;
         }
         //create a cookie with the token to be used signing in 
         public void SetToken(string token)
         {
-            _accessot.HttpContext?.Response.Cookies.Append(SD.TockenCookie, token);
+            this._accessor.HttpContext?.Response.Cookies.Append(SD.TockenCookie, token);
         }
     }
 }
