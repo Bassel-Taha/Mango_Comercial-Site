61e547baf0ce95ee57753aaf7036f2032924d4ca
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-01-01 20:33:57 +0200

    creating the interface of the imessageservicebus and creating the class emblimenting it using servicebus client

---
+++ EmailServiceBus/IMessageServiceBus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailServiceBus
+{
+    internal interface IMessageServiceBus
+    {
+        Task PublishMessage(string QueueName , object Message);
+    }
+}
