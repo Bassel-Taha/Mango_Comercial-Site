f366a32bd49b82685e91fef0e398e46420c02d17
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-01-05 19:43:27 +0200

    -adding the interface for the servicebusconsumer -emplimenting the start and stop methods for the bus processor -creating the event methods fot the messageprocessingasync and the messageerrorasync for the servicebusProcessor

--- Mango.Services.EmailsAPI/Messaging/ServiceBusProcessor.cs
+++ Mango.Services.EmailsAPI/Messaging/ServiceBusProcessor.cs
@@ -1,8 +1,11 @@
-﻿using Azure.Messaging.ServiceBus;
+﻿using System.Text;
+using Azure.Messaging.ServiceBus;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Newtonsoft.Json;
 
 namespace Mango.Services.EmailsAPI.Messaging
 {
-    public class ServiceBusProcessor
+    public class ServiceBusProcessor : IServiceBusProcessor
     {
         public string ConnectionString { get; set; } =
             "Endpoint=sb://mangowebbt.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=YcC2C+YeRZzGLS2SDnqBYyhD/HeykIHLg+ASbEaL6iI="; 
@@ -15,7 +18,48 @@ namespace Mango.Services.EmailsAPI.Messaging
         {
             var client = new ServiceBusClient(ConnectionString);
              _Processor = client.CreateProcessor(QueueName);
-             _Processor.
         }
+
+        // the stop method for the processor to stop receiving messagers from the servicebus
+        public async Task Stop()
+        {
+            await _Processor.StartProcessingAsync(); 
+            await _Processor.DisposeAsync();
+        }
+
+        //the method to start receiving messages from the service bus 
+        public async Task Start()
+        {
+            _Processor.ProcessMessageAsync += OnCartReqeuestRecived;
+            _Processor.ProcessErrorAsync += ErrorHandler;
+        }
+
+        
+        #region the messages delegate events to receive the messages from the bus
+
+        private async Task OnCartReqeuestRecived(ProcessMessageEventArgs arg)
+        {
+            var Message =  arg.Message;
+            var DecodedMessage = Encoding.UTF8.GetString(Message.Body);
+            var DeserializedMessage = JsonConvert.DeserializeObject<CartDto>(DecodedMessage);
+            try
+            {
+                //ToDo try to log the email
+                await arg.CompleteMessageAsync(arg.Message);
+            }
+            catch (Exception e)
+            {
+                throw;
+
+            }
+        }
+
+        private Task ErrorHandler(ProcessErrorEventArgs arg)
+        {
+            Console.WriteLine(arg.Exception);
+            return Task.CompletedTask;
+        }
+
+        #endregion
     }
 }
