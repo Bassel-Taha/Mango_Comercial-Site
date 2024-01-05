79fd04757da1390eeb129d0387d101724432a331
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-01-06 00:53:32 +0200

    -adding the Extention Method  on the IApplicationBuilder and using the IHostApplicationLifeTime to call the Start Method and Stop Memthod in the IServiceBusProcessor to start the service with the startup of the app and the stop with the closing of the app

--- Mango.Services.EmailsAPI/Program.cs
+++ Mango.Services.EmailsAPI/Program.cs
@@ -1,4 +1,6 @@
 using Mango.Services.EmailsAPI.Data;
+using Mango.Services.EmailsAPI.extention_classes;
+using Mango.Services.EmailsAPI.Messaging;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +14,8 @@ builder.Services.AddSwaggerGen();
 //adding the EF to the pipeline 
 builder.Services.AddDbContext<EmailDBContext>(options =>
     options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"]));
-
+//adding the IServiceBusProcessor to the pipeline
+builder.Services.AddSingleton<IServiceBusProcessor, ServiceBusProcessor>();
 
 var app = builder.Build();
 
@@ -26,7 +29,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+//adding the extenstion method tp the pipeLine so that the app can run it with the api startup and close with the app closing
+app.ServiceBusExtentionApplicationBuilder();
 app.MapControllers();
 
 app.Run();
---
+++ Mango.Services.EmailsAPI/extention classes/ServiceBusExtentionMethod.cs
@@ -0,0 +1,28 @@
+﻿using Mango.Services.EmailsAPI.Messaging;
+
+namespace Mango.Services.EmailsAPI.extention_classes
+{
+    public static class ServiceBusExtentionMethod 
+    {
+        private static IServiceBusProcessor _serviceBusProcessor { get; set; }
+
+        public static IApplicationBuilder  ServiceBusExtentionApplicationBuilder (this IApplicationBuilder app)
+        {
+            _serviceBusProcessor =  app.ApplicationServices.GetService<IServiceBusProcessor>();
+            var host = app.ApplicationServices.GetService<IHostApplicationLifetime>();
+            host.ApplicationStarted.Register(OnStart);
+            host.ApplicationStopped.Register(OnStop);
+            return app;
+        }
+
+        private static void OnStop()
+        {
+            _serviceBusProcessor.Start();
+        }
+
+        private static void OnStart()
+        {
+            _serviceBusProcessor.Stop();
+        }
+    }
+}
