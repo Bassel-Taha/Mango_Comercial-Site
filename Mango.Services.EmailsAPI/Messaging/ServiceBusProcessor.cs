1e80cd930f72f4f04037f6d2ba0006dcf643e481
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-01-05 19:02:33 +0200

    ceating the service bus consumer class and the adding the connection string and the queue name to the class and the creating the servicebusprocessor but still need to implement the process message and the process error events

---
+++ Mango.Services.EmailsAPI/Messaging/ServiceBusProcessor.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace Mango.Services.EmailsAPI.Messaging
+{
+    public class ServiceBusProcessor
+    {
+        public string ConnectionString { get; set; } =
+            "Endpoint=sb://mangowebbt.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=YcC2C+YeRZzGLS2SDnqBYyhD/HeykIHLg+ASbEaL6iI="; 
+        
+        public string QueueName { get; set; } = "mangoemailsurvicebus";
+        
+        public Azure.Messaging.ServiceBus.ServiceBusProcessor? _Processor { get; set; }  
+
+        public ServiceBusProcessor()
+        {
+            var client = new ServiceBusClient(ConnectionString);
+             _Processor = client.CreateProcessor(QueueName);
+             _Processor.
+        }
+    }
+}
