7299b3b463ff0cafcdfb36a8c9fec472e3c11628
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-01-12 13:58:44 +0200

    continue with the configes to get the products from the product api

---
+++ Mango.Services.OrderAPI/Services/IServices/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using Mango.Services.OrderAPI.Models.DTOs;
+
+namespace Mango.Services.OrderAPI.Services.IServices
+{
+    interface IProductService
+    {
+        public  Task <ProductsDto> GetAllProducts() ;
+    }
+}
---
+++ Mango.Services.OrderAPI/Services/ProductService.cs
@@ -0,0 +1,19 @@
+﻿using Mango.Services.OrderAPI.Models.DTOs;
+using Mango.Services.OrderAPI.Services.IServices;
+
+namespace Mango.Services.OrderAPI.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IHttpClientFactory _client;
+
+        public ProductService(IHttpClientFactory client)
+        {
+            _client = client;
+        }
+        public Task<ProductsDto> GetAllProducts()
+        {
+            var client = _client.CreateClient("Products");
+        }
+    }
+}
